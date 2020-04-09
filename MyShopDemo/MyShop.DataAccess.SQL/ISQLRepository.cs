028be6c5f7185fef1c32b7d6b332743999aa551e
callsignalpha <62855186+callsignalpha@users.noreply.github.com>
2020-04-08 21:10:02 -0500

    7.Entity changes

---
+++ MyShopDemo/MyShop.DataAccess.SQL/ISQLRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.SQL
+{
+    public interface ISQLRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> Collection();
+        void Commit();
+        void Delete(string Id);
+        T Find(string Id);
+        void Insert(T t);
+        void Update(T t);
+    }
+}
