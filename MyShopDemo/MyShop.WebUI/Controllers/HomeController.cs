8e9ea1ae04cd36539b21c9b2ecfbc0ef287699a7
callsignalpha <62855186+callsignalpha@users.noreply.github.com>
2020-04-08 23:16:37 -0500

    Product Lists to cutomers

--- MyShopDemo/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShopDemo/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,18 +1,45 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Models;
 
 namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        // create instnce of repository
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        //create constructor to intialize the repository
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
         {
-            return View();
+            context = productContext;
+            productCategories = productCategoryContext;
+
         }
 
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
